98979dc625cb9543a26cb23d71ad433d378bcb98
Glade Lynch <blooblahguy@gmail.com>
2019-08-12 12:12:45 -0600

    vanilla compatibility fixes

--- core/compatibility.lua
+++ core/compatibility.lua
@@ -6,29 +6,34 @@ local noob = CreateFrame("frame", nil, UIParent)
 --====================================================
 -- VANILLA
 --====================================================
---globals
-ATTACK_BUTTON_FLASH_TIME = 0.1
+if (bdUI:get_game_version() == "vanilla") then
+	-- globals
+	ATTACK_BUTTON_FLASH_TIME = ATTACK_BUTTON_FLASH_TIME or 0.4
 
--- functions
-CanExitVehicle = CanExitVehicle or noop
-SetSortBagsRightToLeft = SetSortBagsRightToLeft or noop
-UnitGroupRolesAssigned = UnitGroupRolesAssigned or noop
-UnitThreatSituation = UnitThreatSituation or noop
-UnitCastingInfo = UnitCastingInfo or noop
-UnitChannelInfo = UnitChannelInfo or noop
+	-- functions
+	CanExitVehicle = CanExitVehicle or noop
+	SetSortBagsRightToLeft = SetSortBagsRightToLeft or noop
+	UnitGroupRolesAssigned = UnitGroupRolesAssigned or noop
+	UnitThreatSituation = UnitThreatSituation or noop
+	UnitCastingInfo = UnitCastingInfo or noop
+	UnitChannelInfo = UnitChannelInfo or noop
 
--- frames
-ChatFrameMenuButton = ChatFrameMenuButton or noob
-QuickJoinToastButton = QuickJoinToastButton or noob
-ChatFrameChannelButton = ChatFrameChannelButton or noob
-BNToastFrame = BNToastFrame or noob
-ObjectiveTrackerFrame = QuestWatchFrame or noob
-MiniMapInstanceDifficulty = MiniMapInstanceDifficulty or noob
-GarrisonLandingPageMinimapButton = GarrisonLandingPageMinimapButton or noob
-QueueStatusMinimapButton = QueueStatusMinimapButton or noob
-MiniMapTracking = MiniMapTracking or noob
-SpellFlyout = SpellFlyout or noob
-BagItemSearchBox = BagItemSearchBox or noob
-BagItemAutoSortButton = BagItemAutoSortButton or noob
-BankItemAutoSortButton = BankItemAutoSortButton or noob
-BankItemSearchBox = BankItemSearchBox or noob
+	-- frames
+	ChatFrameMenuButton = ChatFrameMenuButton or noob
+	QuickJoinToastButton = QuickJoinToastButton or noob
+	ChatFrameChannelButton = ChatFrameChannelButton or noob
+	BNToastFrame = BNToastFrame or noob
+	ObjectiveTrackerFrame = ObjectiveTrackerFrame or QuestWatchFrame or noob
+	MiniMapInstanceDifficulty = MiniMapInstanceDifficulty or noob
+	GarrisonLandingPageMinimapButton = GarrisonLandingPageMinimapButton or noob
+	QueueStatusMinimapButton = QueueStatusMinimapButton or noob
+	MiniMapTracking = MiniMapTracking or noob
+	BagItemSearchBox = BagItemSearchBox or noob
+	BagItemAutoSortButton = BagItemAutoSortButton or noob
+	BankItemAutoSortButton = BankItemAutoSortButton or noob
+	BankItemSearchBox = BankItemSearchBox or noob
+end
+
+--====================================================
+-- TBC
+--====================================================
