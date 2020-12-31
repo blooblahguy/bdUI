f0ec539f89dd50b6e340948c3c312ca425019199
blooblahguy <blooblahguy@gmail.com>
2020-12-30 19:38:00 -0700

    fix arcane color

--- modules/unitframes/init.lua
+++ modules/unitframes/init.lua
@@ -369,6 +369,9 @@ function mod:initialize()
 
 	bdUI.oUF.colors.power[0] = {46/255, 130/255, 215/255}
 	bdUI.oUF.colors.power["MANA"] = {46/255, 130/255, 215/255}
+
+	bdUI.oUF.colors.power.ARCANE_CHARGES = {55/255, 139/255, 224/255}
+	
 	mod:create_unitframes()
 
 	mod:config_callback()
