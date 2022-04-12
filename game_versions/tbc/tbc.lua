6f7124c3b514b506e032879b1ccd5e949fd8590f
Glade Lynch <blooblahguy@gmail.com>
2022-04-12 10:33:28 -0600

    removing tbc override file for possible taint

--- game_versions/tbc/tbc.lua
+++ game_versions/tbc/tbc.lua
@@ -19,9 +19,6 @@ local noob = CreateFrame("frame", nil, UIParent)
 -- end
 
 -- globals
-ATTACK_BUTTON_FLASH_TIME = ATTACK_BUTTON_FLASH_TIME or 0.4
-
--- functions
 -- GetSpecializationInfoByID = GetSpecializationInfoByID or noop
 
 -- UnitThreatSituation = UnitThreatSituation or noop
@@ -32,33 +29,33 @@ ATTACK_BUTTON_FLASH_TIME = ATTACK_BUTTON_FLASH_TIME or 0.4
 -- GetSpecialization = GetSpecialization or noop
 
 -- frames
-MiniMapTrackingDropDown = MiniMapTrackingDropDown or noob
-SpellFlyout = SpellFlyout or noob
-ClassNameplateManaBarFrame = TalentMicroButtonAlert or noob
-TalentMicroButtonAlert = TalentMicroButtonAlert or noob
-ChatFrameMenuButton = ChatFrameMenuButton or noob
-QuickJoinToastButton = QuickJoinToastButton or noob
-ChatFrameChannelButton = ChatFrameChannelButton or noob
-BNToastFrame = BNToastFrame or noob
-ObjectiveTrackerFrame = ObjectiveTrackerFrame or QuestWatchFrame or noob
-MiniMapInstanceDifficulty = MiniMapInstanceDifficulty or noob
-GarrisonLandingPageMinimapButton = GarrisonLandingPageMinimapButton or noob
-QueueStatusMinimapButton = QueueStatusMinimapButton or noob
-MiniMapTracking = MiniMapTracking or MiniMapTrackingFrame or noob
--- BagItemSearchBox = BagItemSearchBox or noob
-BagItemAutoSortButton = BagItemAutoSortButton or noob
-BankItemAutoSortButton = BankItemAutoSortButton or noob
--- BankItemSearchBox = BankItemSearchBox or noob
-ReagentBankFrame = ReagentBankFrame or noob
-BankFrameMoneyFrameInset = BankFrameMoneyFrameInset or noob
-BackpackTokenFrame = BackpackTokenFrame or noob
-BankFrameCloseButton = BankFrameCloseButton or noob
-CharacterMicroButtonAlert = CharacterMicroButtonAlert or noob
-TalentMicroButtonAlert = TalentMicroButtonAlert or noob
-MiniMapTrackingButtonBorder = MiniMapTrackingButtonBorder or noob
-MiniMapTrackingButtonShine = MiniMapTrackingButtonShine or noob
-QueueStatusMinimapButton = QueueStatusMinimapButton or noob
-QueueStatusMinimapButtonIcon = QueueStatusMinimapButtonIcon or noob
+-- MiniMapTrackingDropDown = MiniMapTrackingDropDown or noob
+-- SpellFlyout = SpellFlyout or noob
+-- ClassNameplateManaBarFrame = TalentMicroButtonAlert or noob
+-- TalentMicroButtonAlert = TalentMicroButtonAlert or noob
+-- ChatFrameMenuButton = ChatFrameMenuButton or noob
+-- QuickJoinToastButton = QuickJoinToastButton or noob
+-- ChatFrameChannelButton = ChatFrameChannelButton or noob
+-- BNToastFrame = BNToastFrame or noob
+-- ObjectiveTrackerFrame = ObjectiveTrackerFrame or QuestWatchFrame or noob
+-- MiniMapInstanceDifficulty = MiniMapInstanceDifficulty or noob
+-- GarrisonLandingPageMinimapButton = GarrisonLandingPageMinimapButton or noob
+-- QueueStatusMinimapButton = QueueStatusMinimapButton or noob
+-- MiniMapTracking = MiniMapTracking or MiniMapTrackingFrame or noob
+-- -- BagItemSearchBox = BagItemSearchBox or noob
+-- BagItemAutoSortButton = BagItemAutoSortButton or noob
+-- BankItemAutoSortButton = BankItemAutoSortButton or noob
+-- -- BankItemSearchBox = BankItemSearchBox or noob
+-- ReagentBankFrame = ReagentBankFrame or noob
+-- BankFrameMoneyFrameInset = BankFrameMoneyFrameInset or noob
+-- BackpackTokenFrame = BackpackTokenFrame or noob
+-- BankFrameCloseButton = BankFrameCloseButton or noob
+-- CharacterMicroButtonAlert = CharacterMicroButtonAlert or noob
+-- TalentMicroButtonAlert = TalentMicroButtonAlert or noob
+-- MiniMapTrackingButtonBorder = MiniMapTrackingButtonBorder or noob
+-- MiniMapTrackingButtonShine = MiniMapTrackingButtonShine or noob
+-- QueueStatusMinimapButton = QueueStatusMinimapButton or noob
+-- QueueStatusMinimapButtonIcon = QueueStatusMinimapButtonIcon or noob
 
 -- threat functions
 -- local threatcolors = {
