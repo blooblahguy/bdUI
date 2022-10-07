f976e86687fa636b6fec60b7358c92edc4f8b480
blooblahguy <blooblahguy@gmail.com>
2022-10-07 02:16:59 -0600

    wotlk aura

--- game_versions/wotlk/wotlk_auras.lua
+++ game_versions/wotlk/wotlk_auras.lua
@@ -1,1 +1,14 @@
--- test
+local bdUI, c, l = unpack(select(2, ...))
+
+local raidauras = {}
+local special = {}
+
+
+
+-- lowercase these
+raidauras = bdUI:lowercase_table(raidauras)
+special = bdUI:lowercase_table(special)
+
+-- merge tables
+for k,v in pairs(raidauras) do bdUI.aura_lists.raid[k] = v end
+for k,v in pairs(special) do bdUI.aura_lists.special[k] = v end
