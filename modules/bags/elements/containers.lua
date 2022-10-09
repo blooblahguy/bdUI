1cdc06b7ff7cf8db088b597d9215a63bd5f7ccbd
Baine <Baine@users.noreply.github.com>
2022-10-09 09:27:35 +0200

    Update containers.lua

--- modules/bags/elements/containers.lua
+++ modules/bags/elements/containers.lua
@@ -113,7 +113,7 @@ function mod:create_container(name, nomove)
 
 		for k, item in pairs(frame.all_items) do
 			if (item.name) then
-				local text = search_sanitize(item.name..item.itemType..item.bindType..item.tradeable
+				local text = search_sanitize(item.name..item.itemType..item.bindType..item.tradeable)
 				if(not item.itemType) then item.itemType=13 end				
         
 				if (find == "boe") then
