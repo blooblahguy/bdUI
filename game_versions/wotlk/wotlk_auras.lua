ff7e0c8df4e06c48531867aca7b514f02b98c6fb
blooblahguy <blooblahguy@gmail.com>
2022-10-18 21:25:29 -0600

    adding some basic auras from the raids

--- game_versions/wotlk/wotlk_auras.lua
+++ game_versions/wotlk/wotlk_auras.lua
@@ -3,6 +3,29 @@ local bdUI, c, l = unpack(select(2, ...))
 local raidauras = {}
 local special = {}
 
+-- general debuffs
+raidauras["Ardent Defender"] = true
+
+-- naxx debuffs
+raidauras["Necrotic Aura"] = true
+raidauras["Fungal Creep"] = true
+raidauras["Mortal Wound"] = true
+raidauras["Chill"] = true
+raidauras["Silence"] = true
+raidauras["Mortal Wound"] = true
+raidauras["Rain of Fire"] = true
+raidauras["Web Wrap"] = true
+special["Web Wrap"] = true
+raidauras["Mind Control"] = true
+special["Mind Control"] = true
+raidauras["Flame Tsunami"] = true
+special["Flame Tsunami"] = true
+raidauras["Icebolt"] = true
+raidauras["Frost Blast"] = true
+special["Frost Blast"] = true
+raidauras["Chains of Kel'Thuzad"] = true
+special["Chains of Kel'Thuzad"] = true
+raidauras["Tail Lash"] = true
 
 
 -- lowercase these
