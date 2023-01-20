7a82d8801682004477a525f900d4a76e2ab86b1b
blooblahguy <blooblahguy@gmail.com>
2023-01-19 21:21:57 -0700

    renaming vehicle frame

--- modules/general/elements/vehicles.lua
+++ modules/general/elements/vehicles.lua
@@ -14,7 +14,7 @@ function mod:move_vehicles()
 	local vehicle = _G.VehicleSeatIndicator
 	vehicle:ClearAllPoints()
 	vehicle:SetPoint("TOPRIGHT", Minimap, "BOTTOMLEFT", -10, -10)
-	bdMove:set_moveable(vehicle)
+	bdMove:set_moveable(vehicle, "Vehicle Frame")
 
 	hooksecurefunc(vehicle, 'SetPoint', set_position)
 	-- hooksecurefunc('VehicleSeatIndicator_SetUpVehicle', VehicleSetUp)
